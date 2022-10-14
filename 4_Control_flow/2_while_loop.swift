a3540dedf7cdcf0e27771525fa4fddde21a9eb74
Chethana N P <chethana.np@cumulations.com>
2022-10-14 19:25:18 +0530

    while loop

--- 4_Control_flow/2_while_loop.swift
+++ 4_Control_flow/2_while_loop.swift
@@ -1,7 +1,7 @@
 //while loop checks a condition initially if condition is false loop ends
 
 let destination = 25
-let boardOnGoing = 0
+var boardOnGoing = 0
 var boardNumbers = [Int](repeating: 0, count: destination + 1)
 var diceRoll = Int()
 
@@ -14,7 +14,19 @@ boardNumbers[19] = -11
 boardNumbers[22] = -2
 boardNumbers[24] = -8
 
-while (boardOnGoing < destination){
-    
+while (boardOnGoing < destination)
+{
+    diceRoll = diceRoll + 1
+    if diceRoll == 7 {
+        diceRoll = 1
+    }
+    boardOnGoing = boardOnGoing + diceRoll
+
+    if boardOnGoing < boardNumbers.count{
+        boardOnGoing = boardOnGoing + boardNumbers[boardOnGoing]
+        print(boardOnGoing)
+        print("You are just \(destination - boardOnGoing) steps away from destination")
+    }
 }
+print("congrajulations")
 
